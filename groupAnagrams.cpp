be37296815351fb8062e4907beecd30c0affca48
tianxin1860 <15626487296@163.com>
2016-03-02 15:23:56 +0800

    indent

--- groupAnagrams.cpp
+++ groupAnagrams.cpp
@@ -20,27 +20,27 @@ For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
 
 
 class Solution {
-		public:
-				vector<vector<string>> groupAnagrams(vector<string>& strs) {
-						vector< vector<string> > result;
-						vector<string> tmp;
-						unordered_map<string, vector<int> > map;
-						for(int i=0; i<strs.size(); i++) {
-								string key = strs[i];
-								sort(key.begin(), key.end());
-								map[key].push_back(i);
-						}
-						for(unordered_map<string, vector<int> >::iterator it = map.begin(); it!=map.end(); it++) {
-								vector<int> index = (*it).second;
-								for(vector<int>::iterator it = index.begin(); it!=index.end(); it++) {
-										tmp.push_back(strs[*it]);
-								}
-								sort(tmp.begin(), tmp.end());
-								result.push_back(tmp);
-								tmp.clear();
-						}
-						return result;
+	public:
+		vector<vector<string>> groupAnagrams(vector<string>& strs) {
+			vector< vector<string> > result;
+			vector<string> tmp;
+			unordered_map<string, vector<int> > map;
+			for(int i=0; i<strs.size(); i++) {
+				string key = strs[i];
+				sort(key.begin(), key.end());
+				map[key].push_back(i);
+			}
+			for(unordered_map<string, vector<int> >::iterator it = map.begin(); it!=map.end(); it++) {
+				vector<int> index = (*it).second;
+				for(vector<int>::iterator it = index.begin(); it!=index.end(); it++) {
+					tmp.push_back(strs[*it]);
 				}
+				sort(tmp.begin(), tmp.end());
+				result.push_back(tmp);
+				tmp.clear();
+			}
+			return result;
+		}
 };
 
 
